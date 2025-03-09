7113a298ee7d69058916991d7aafad1bd0c99014
yemiaro <yemiarole@icloud.com>
2025-03-09 21:01:39 +0000

    mobile navbar functionality

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,5 +1,66 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  {
+    name: "home",
+    href: "/",
+  },
+  {
+    name: "services",
+    href: "/services",
+  },
+  {
+    name: "CV",
+    href: "/cv",
+  },
+  {
+    name: "work",
+    href: "/work",
+  },
+  {
+    name: "contact",
+    href: "/contact",
+  },
+];
+
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Yemi<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.href}
+                key={index}
+                className={`${link.href === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
