dde9c406a37b62816e1ccd65c7dc010c43c55474
yemiaro <yemiarole@icloud.com>
2025-03-09 14:29:55 +0000

    initiating portfolio website using react and nextjs

---
+++ components/Header.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// components
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Yemi <span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* desktop nav */}
+        <Nav />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
