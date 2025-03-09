d18e32cd275f1d407496c08b219bd971f9d3ea89
yemiaro <yemiarole@icloud.com>
2025-03-09 20:10:23 +0000

    added stair animation to transition through pages

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,5 +1,33 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+// components
+import Stairs from "./Stairs";
+
 const StairTransition = () => {
-  return <div>StairTransition</div>;
+  const pathname = usePathname();
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        <div key={pathname}>
+          <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+            <Stairs />
+          </div>
+
+          <motion.div
+            className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+            initial={{ opacity: 1 }}
+            animate={{
+              opacity: 0,
+              tranisition: { delay: 1, duration: 0.4, ease: "EasInOut" },
+            }}
+          />
+        </div>
+      </AnimatePresence>
+    </>
+  );
 };
 
 export default StairTransition;
