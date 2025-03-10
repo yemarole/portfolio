84cbaf61abb4d87999950a0e92259927e1b09ea5
yemiaro <yemiarole@icloud.com>
2025-03-10 22:07:57 +0000

    created services page, a few troubleshoots on hovering features noted in file

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,100 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "I build websites that serve as powerful marketing tools and bring memorable brand experiences.",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      "I focus on creating a seamless experience across all devices and platforms.",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Backend Development",
+    description:
+      "I build fast, scalable, and secure custom backend systems using the latest technologies.",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "AI Driven Development",
+    description:
+      "I build AI-driven solutions that enable game-changing business outcomes.",
+    href: "",
+  },
+];
+
+import { delay, motion } from "framer-motion";
+
 const Services = () => {
-  return <div>services page</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-1 flex flex-col justify-center gap-6 group"
+                // issue with the hover effect which resulted in using onMouseEnter and onMouseLeave
+                onMouseEnter={(e) => {
+                  e.currentTarget.querySelector(
+                    ".service-number"
+                  ).style.WebkitTextStroke = "1px #00ff99";
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.querySelector(
+                    ".service-number"
+                  ).style.WebkitTextStroke = "1px #ffffff";
+                }}
+              >
+                {/* top */}
+                <div className="w-full flex justify-between items-center">
+                  <div
+                    className="text-5xl font-extrabold text-transparent text-outline transition-all duration-500 service-number"
+                    style={{ WebkitTextStroke: "1px #ffffff" }}
+                  >
+                    {service.num}
+                  </div>
+                  <Link
+                    href={service.href}
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
+                  </Link>
+                </div>
+                {/* heading */}
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </h2>
+                {/* description */}
+                <p className="text-white/60 ">{service.description}</p>
+                {/* border */}
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
