fd52b061f7bc01c93619bdb98b580e2a6af4dfdb
yemiaro <yemiarole@icloud.com>
2025-03-17 14:31:36 +0000

    sending updated source

--- app/cv/page.jsx
+++ app/cv/page.jsx
@@ -1,3 +1,58 @@
+"use client";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaPython,
+  FaNodeJs,
+} from "react-icons/fa";
+
+import { SiTailwindcss } from "react-icons/si";
+
+// about data
+const about = {
+  title: "About me",
+  description:
+    "I'm an aspiring full-stack developer with a passion for web development. I have experience in building web applications using modern technologies like React, Node.js, and Python. I'm always eager to learn new things and improve my skills.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Yemi Arole",
+    },
+  ],
+  info: [
+    {
+      fieldName: "Email",
+      fieldValue: "yemiarole@icloud.com",
+    },
+  ],
+  info: [
+    {
+      fieldName: "Experience",
+      fieldValue: "1+ years",
+    },
+  ],
+  info: [
+    {
+      fieldName: "Github",
+      fieldValue: "@yemaro",
+    },
+  ],
+  info: [
+    {
+      fieldName: "Status",
+      fieldValue: "Available",
+    },
+  ],
+};
+
+// experience data
+const experience = {
+  icon: "",
+};
+
 const CV = () => {
   return <div>CV page</div>;
 };
