7f54cc7d1848a9fba87fce1d78205fe59d130399
yemiaro <yemiarole@icloud.com>
2025-03-19 22:12:31 +0000

    changed the background of the stair animation to a green gradient

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -21,7 +21,7 @@ const StairTransition = () => {
             initial={{ opacity: 1 }}
             animate={{
               opacity: 0,
-              tranisition: { delay: 1, duration: 0.4, ease: "EasInOut" },
+              transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
             }}
           />
         </div>
