401b71753ef61de44e5046cb315a58daf3d1f43f
yemiaro <yemiarole@icloud.com>
2025-03-20 03:16:17 +0000

    started on the contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,65 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "07123458789",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "myemail@email.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Code Corner, Tech Town EC3V 2AN",
+  },
+];
+
+import { motion } from "framer-motion";
+
 const Contact = () => {
-  return <div>contact page</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form action="">form</form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-8">
+            info
+          </div>
+        </div>
+      </div>{" "}
+    </motion.section>
+  );
 };
 
 export default Contact;
